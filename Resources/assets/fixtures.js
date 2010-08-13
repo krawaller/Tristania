70949f84997161fdc27aa99b07375c3ecbd74466
KungD <david@krawaller.se>
2010-08-13 12:55:56 +0200

    adding the fixtures file

---
+++ Resources/assets/fixtures.js
@@ -0,0 +1,49 @@
+    var fixtures = {
+        comments: {
+            widowsweeds: [{
+                by: "ole",
+                content: "<p>I like this album. Very different from what we do now, but still!</p>"
+            },{
+                by: "anders",
+                content: "<p>But it shows that we were almost teenagers at the time...</p>"
+            }],
+            evenfall: [{
+                by: "einar",
+                content: "<p>The video they made for this track is absolutely horrid...</p>"
+            }]
+        },
+        presentations: {
+            widowsweeds: "<p>First full length album, which defined the gothic metal genre.</p>",
+            beyondtheveil: "<p>Continuing the path embarked upon with Widow's Weeds, Beyond the Veil secured Tristania's claim to the gothic metal throne.</p>",
+            worldofglass: "<p>With World of Glass, Tristania proved they were still standing strong in spite of Veland's departure.</p>",
+            ashes: "<p>The long-awaited World of Glass followup showed a more mature Tristania, yet still retaining a gothic undercurrent.</p>",
+            illumination: "<p>Yet again Tristania releases a different but similar album, with new musical approaches but still somehow woven from the same cloth.</p>",
+            rubicon: "<p>With Rubicon Tristania enters a new era, where many of the new members contribute actively to the composing. This will also be the album premiere for Mariangela, new singer after Vibeke's departure.</p>",
+            ole: "<p>Ole inherited Rune's bass responsibilities, but have quickly also become an important cog in the composing procedure. He also does backing vocals.</p>"
+        },
+        selectedvideos: [{
+            title: "Karmøygeddon - Year of the Rat",
+            id: "wSQ9TnIFthA"
+        }],
+        officialvideos: [{
+            title: "Year of the Rat",
+            id: "0h6kauOYnGM"
+        }],
+        photoalbums: [{
+            title: "Rubicon promo shoot",
+            id: 195
+        }],
+        news: [{
+            date: "2010-07-16",
+            title: "New app version",
+            content: "<p>A new version of the app is out! Hit the app store and update!</p>"
+        },{
+            date: "2010-07-02",
+            title: "Rubicon release party",
+            content: "<p>Make sure you're in Stavanger on september 25th!</p><p>More info blah blah etc etc aso aso.</p>"
+        },{
+            date: "2010-06-15",
+            title: "Second Rubicon teaser",
+            content: "<p>Check out the official youtube channel (just hit the tab right here in the app!) for a new Rubicon teaser!</p><p>This time featuring the previously announced work of the illustrious Pete Johanssen.</p>"
+        }]
+    }
