90f9fff393bb5569fa506ab9a7558a8256c4994f
KungD <david@krawaller.se>
2010-08-30 17:45:43 +0200

    enabled removing of fav album

--- Resources/main_windows/discography.js
+++ Resources/main_windows/discography.js
@@ -14,8 +14,24 @@ function setFav(e){
 
 var albums = $.getAlbums(),
     images = albums.map(function(a){ return "../pics/"+a.pic; }),
-    addfav = $.create({ type: "Button", image: '../pics/icon_unstar.png', click: setFav }),
-    delfav = $.create({ type: "Button", image: '../pics/icon_star.png', click: setFav}),
+    addfav = $.create({
+        type: "Button",
+        image: '../pics/icon_unstar.png', 
+        click: function(e){
+            Ti.UI.createAlertDialog({ title: 'Album selected', message: 'Favourite album set to '+albums[cfv.selected].title }).show();
+            Ti.App.Properties.setString("favalbum",albums[cfv.selected].shorttitle);
+            updateView();
+        }
+    }),
+    delfav = $.create({
+        type: "Button",
+        image: '../pics/icon_star.png', 
+        click: function(e){
+            Ti.UI.createAlertDialog({ title: 'Favourite cleared', message: "No favourite album set" }).show();
+            Ti.App.Properties.setString("favalbum","");
+            updateView();
+        }
+    }),
     cfv = $.create({ 
         type: "CoverFlowView",
         images:images,
